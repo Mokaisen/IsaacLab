10641c922c217cf557b898c5faf84574fd642b23
Cesar Molano <cesarmolano@192.168.1.6>
2025-03-05 11:48:47 -0500

    Initial changes to try RL with brownbot

---
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/brownbot.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Universal Robots of brownbotics.
+
+Reference: https://github.com/ros-industrial/universal_robot
+"""
+
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.actuators import ImplicitActuatorCfg
+from omni.isaac.lab.assets.articulation import ArticulationCfg
+from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+
+BROWNBOT05_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/isaac_sim/workspaces/isaac_sim_scene/UR5_brown.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.712,
+            "elbow_joint": 1.712,
+            "wrist_1_joint": 0.0,
+            "wrist_2_joint": 0.0,
+            "wrist_3_joint": 0.0,
+        },
+    ),
+)
+"""Configuration of UR-10 arm using implicit actuator models."""
